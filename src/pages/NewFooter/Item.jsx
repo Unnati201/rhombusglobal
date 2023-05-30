5b290f833c48665d7fec52b9d66505774e66e64d
Unnati201 <unnatisingh387@gmail.com>
2023-05-30 12:43:44 +0530

    Tuesday2

--- src/pages/NewFooter/Item.jsx
+++ src/pages/NewFooter/Item.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Item = ({ Links, title }) => {
   return (
     <ul>
-      <h1 className="mb-2 font-semibold ml-20 text-2xl font-mulish ">{title}</h1>
+      <h1 className="mb-2 font-semibold ml-10 text-2xl font-mulish ">{title}</h1>
       {Links.map((link) => (
         <li key={link.name}>
           <a
             className="hover:text-[#007bff] duration-300
-          text-sm  ml-20 cursor-pointer leading-7"
+          text-sm  ml-10 cursor-pointer leading-7"
             href={link.link}
           >
             {link.name}
